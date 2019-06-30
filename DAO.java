9a6dacf66699bdf0887461f308f949dccfabda1a
MIkeXIV <41412066+MIkeXIV@users.noreply.github.com>
2019-06-29 20:13:51 -0400

    Add files via upload
    fixed some bugs and program runs

---
+++ DAO.java
@@ -0,0 +1,108 @@
+package dao;
+
+import java.util.*;
+import models.Author;
+import models.Book;
+import models.Publisher;
+
+import java.io.*;
+
+public class DAO {
+	
+
+		public static Scanner inputStream;
+
+		public static ArrayList<Author> authorList = new ArrayList<Author>(1000);
+		public static ArrayList<Book> bookList = new ArrayList<Book>(1000);
+		public static ArrayList<Publisher> publisherList = new ArrayList<Publisher>(1000);
+
+		public void initialize() {
+
+			String currentLine = "";
+			int max = 1000;
+			int min = 1;
+			int range = max - min + 1;
+
+			try {
+				inputStream = new Scanner(new FileInputStream("C:\\Users\\Mike\\eclipse-workspace\\libraryManagementSystem\\src\\data\\Books.csv"));
+			}
+
+			catch (FileNotFoundException e) {
+				System.out.println("Something has happened and Java cannot find (or open) the input file selected.");
+			}
+			
+			while (inputStream.hasNextLine()) {
+
+				currentLine = inputStream.nextLine();
+
+				String[] delimitedStrings = currentLine.split(",");
+				
+				String bookID = delimitedStrings[0];
+				int authorID = Integer.parseInt(delimitedStrings[1]);
+				int publisherID = Integer.parseInt(delimitedStrings[2]);
+				String bookTitle = delimitedStrings[3];
+				
+				Book book = new Book(bookID, authorID, publisherID, bookTitle);
+				bookList.add(book);
+				/*
+				String authorName = delimitedStrings[0];
+				String bookTitle = delimitedStrings[1];
+				String bookID = delimitedStrings[2];
+				String publisherName = delimitedStrings[3];
+				*/	
+			}
+			
+			try {
+				inputStream = new Scanner(new FileInputStream("C:\\Users\\Mike\\eclipse-workspace\\libraryManagementSystem\\src\\data\\Authors.csv"));
+			}
+
+			catch (FileNotFoundException e) {
+				System.out.println("Something has happened and Java cannot find (or open) the input file selected.");
+			}
+			
+			while (inputStream.hasNextLine()) {
+
+				currentLine = inputStream.nextLine();
+
+				String[] delimitedStrings = currentLine.split(",");
+				
+				int authorID = Integer.parseInt(delimitedStrings[0]);
+				String authorName = delimitedStrings[1];				
+				Author author = new Author(authorID, authorName);
+				authorList.add(author);
+				/*
+				String authorName = delimitedStrings[0];
+				String bookTitle = delimitedStrings[1];
+				String bookID = delimitedStrings[2];
+				String publisherName = delimitedStrings[3];
+				*/
+			}
+			
+
+			try {
+				inputStream = new Scanner(new FileInputStream("C:\\Users\\Mike\\eclipse-workspace\\libraryManagementSystem\\src\\data\\Publishers.csv"));
+			}
+
+			catch (FileNotFoundException e) {
+				System.out.println("Something has happened and Java cannot find (or open) the input file selected.");
+			}
+			
+			while (inputStream.hasNextLine()) {
+
+				currentLine = inputStream.nextLine();
+
+				String[] delimitedStrings = currentLine.split(",");
+				
+				int publisherID = Integer.parseInt(delimitedStrings[0]);
+				String publisherName = delimitedStrings[1];
+				Publisher publisher = new Publisher(publisherID, publisherName);
+				publisherList.add(publisher);
+				/*
+				String authorName = delimitedStrings[0];
+				String bookTitle = delimitedStrings[1];
+				String bookID = delimitedStrings[2];
+				String publisherName = delimitedStrings[3];
+				*/				
+			}
+		}
+}
